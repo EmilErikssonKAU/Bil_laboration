7f296e7e9244a9fc5b0f1ecd98f854473dcf82ac
EmilEriksson <emileriksson432@gmail.com>
2023-11-21 14:16:08 +0100

    Sparar till nästa vecka, Sylas ska lära sig pekare

--- src/editVehicles.c
+++ src/editVehicles.c
@@ -1,1 +1,20 @@
-void add 
+#include "funktioner.h"
+#include "structs.h"
+
+/*
+    Every NULL pointer in the array is an empty spot, therefore the first NULL pointer is the
+    first empty spot in the array
+*/
+
+int checkLength(vregister* vreg){
+    int length = 0;
+    while(vreg -> vehicleArr + length != NULL){
+        length++;
+    }
+    return length;
+}
+
+
+void addVehicle(vregister* vreg){
+    int index = checkLength(vreg) + 1;
+}
