b11217a2dceccd97856ffe88cee7be6e854fd2ad
Silas van der Waal <silasvanderwaal@gmail.com>
2023-11-21 13:57:41 +0100

    Function that prints out the menu

---
+++ src/print.c
@@ -0,0 +1,8 @@
+void printMenu(){
+    printf("1. Lägg till ett fordon.\n");
+    printf("2. Ta bort ett fordon.\n");
+    printf("3. Sortering efter bilmärke.\n");
+    printf("4. Skriv ut information om fordon.\n");
+    printf("5. Skriv ut hela fordonsregistret.\n");
+    printf("0. Avsluta programmet.\n");
+}
