e2e13c98e569f02510a0bfe8408e1ddb23a35a8b
EmilEriksson <emileriksson432@gmail.com>
2023-11-21 11:46:18 +0100

    Second commit

---
+++ src/funktioner.h
@@ -0,0 +1,12 @@
+#ifndef FUNKTIONER_H
+#define FUNKTIONER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#define ENTRIES_LENGTH
+#define NAME_LENGTH
+
+#endif
---
+++ src/structs.h
@@ -0,0 +1,11 @@
+typedef struct fregister{
+    fordon fordonArr [MAX_ENTRIES];
+}fregister;
+
+typedef struct fordon{
+    char
+}fordon;
+
+typedef struct person{
+
+}person;
